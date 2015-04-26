d67c726615ecc83424b22c0122909ed3d1b8f63c
Marcos Diez <marcos@unitron.com.br>
2015-04-26 04:47:19 +0000

    communication class is evolving

--- app/src/main/java/com/marcosdiez/spectrumanalyzer/Communication.java
+++ app/src/main/java/com/marcosdiez/spectrumanalyzer/Communication.java
@@ -1,21 +1,87 @@
 package com.marcosdiez.spectrumanalyzer;
 
+import java.util.HashMap;
+
 /**
  * Created by Marcos on 29-Apr-15.
  */
 public class Communication {
-    public static final int BUFFER_SIZE = 1000;
-    static int[] buffer =  new int[BUFFER_SIZE];
+    public static final String alphabet = "; 123456789.abcdefghijklmnopqrstuvwyxz";
+    public static int numWords = 5;  // new base size
 
-    public static String alphabet =" 123456789.;abcdefghijklmnopqrstuvwyxz";
-    public static int numWords = 5;
+    static HashMap<Character, String> letterToEncoded = null;
+    static HashMap<String, Character> encodedToLetter = null;
 
-    public static String fixString(String input){
-        return input.replace(",",".").toLowerCase();
+    public static void init() {
+        if (letterToEncoded != null) {
+            return;
+        }
+        int dictionarySize = alphabet.length();
+
+        letterToEncoded = new HashMap<Character, String>(dictionarySize);
+        encodedToLetter = new HashMap<String, Character>(dictionarySize);
+
+        int counter = 0;
+        for (int i = 0; i < numWords; i++) {
+            for (int j = 0; j < numWords; j++) {
+                for (int k = 0; k < numWords; k++) {
+                    if (i != j && j != k) {
+                        String encodedString = i + "" + j + "" + k;
+                        Character letter = alphabet.charAt(counter);
+
+                        // System.out.println("Adding " + letter + " <--> " + encodedString);
+
+                        letterToEncoded.put(letter, encodedString);
+                        encodedToLetter.put(encodedString, letter);
+
+
+                        counter++;
+                        if (counter == dictionarySize) {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
-    public static int[] toNumbers(String input){
-        return buffer;
+    public static String fixString(String input) {
+        return input.replace(",", ".").toLowerCase();
+    }
+
+    public static String toNewBase(char letter) {
+        init();
+        return letterToEncoded.get(letter);
+    }
+
+    public static char toLetter(String fromBase) {
+        init();
+        return encodedToLetter.get(fromBase);
+    }
+
+    public static void player(String sentence, Beeper b) {
+        int size = sentence.length();
+        for (int i = 0; i < size; i++) {
+            char c = sentence.charAt(i);
+            String newBase = toNewBase(c);
+            playerHelper(newBase, b);
+        }
+    }
+
+    private static void playerHelper(String baseNumber, Beeper b) {
+        int size = baseNumber.length();
+        for (int i = 0; i < size; i++) {
+            char c = baseNumber.charAt(i);
+            b.beepChar(c);
+        }
+        b.beepWordSeparator();
+    }
+
+    public interface Beeper {
+        void beepChar(char c);
+
+        void beepWordSeparator();
     }
 
 }
---
+++ app/src/test/java/CommunicationTest.java
@@ -0,0 +1,62 @@
+import com.marcosdiez.spectrumanalyzer.Communication;
+
+import org.junit.Test;
+
+import dalvik.annotation.TestTargetClass;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommunicationTest {
+
+    @Test
+    public void testFixString() throws Exception {
+        assertEquals("", Communication.fixString(""));
+        assertEquals("the book is on the table", Communication.fixString("The BOOK is On the TablE"));
+    }
+
+//    @Test
+//    public void testPlayerHelper(){
+//        // this does not test anything but it's nice to see the results
+//        // since I don't know yet how many words I will have, verifying the values make no sense
+//        TestBeeper t = new TestBeeper();
+//        System.out.println("-------------------A-");
+//        Communication.playerHelper("123", t);
+//        System.out.println("-------------------B-");
+//    }
+
+    @Test
+    public void testTranslation(){
+        for(int i = 0 ; i < Communication.alphabet.length(); i++ ){
+            char c = Communication.alphabet.charAt(i);
+            String newLetter = Communication.toNewBase(c);
+            // System.out.println("----" + c + " -- " + newLetter + " -- ");
+            char newC = Communication.toLetter(newLetter);
+            assertEquals(c, newC);
+        }
+    }
+
+    @Test
+    public void testPlayer(){
+        // this does not test anything but it's nice to see the results
+        // since I don't know yet how many words I will have, verifying the values make no sense
+        // to see the output,
+        // gradle testDebug -i
+
+        String testString = "the book is on the table 22z.";
+        TestBeeper t = new TestBeeper();
+        System.out.println("-------------------A-");
+        Communication.player(testString, t);
+        System.out.println("-------------------B-");
+    }
+
+    public class TestBeeper implements Communication.Beeper {
+        public void beepChar(char c) {
+            System.out.print(c);
+        }
+
+        public void beepWordSeparator() {
+            System.out.println("!");
+        }
+    }
+
+}
